7296e41d3e3503d16c17b1a0daf3df57f5b9a4b2
penguin_syan <member@penguin-syan.tokyo>
2021-09-30 03:26:39 +0900

    [clean]不要な個所を削除

--- js/script_purchased.js
+++ js/script_purchased.js
@@ -1,12 +1,7 @@
 function audioPlay(){
-   //var audio = new Audio("../../audio/01.ogg");
    var dialog_window = document.getElementById("dialog");
    var audio = document.getElementById('koharu');
 
    audio.play();
-   //var sound = new Howl({ src: ['../../audio/01.ogg'] })
-   
-   //sound.play();
-   //audio.play();
    dialog_window.hidden = "true";
 }
