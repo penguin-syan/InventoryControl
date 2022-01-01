06a20c1a55e2dc8d1c43985dbef409628392e363
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-01-01 18:57:24 +0900

    在庫登録画面のスクリプトを変更

--- admin/addinventory/index.php
+++ admin/addinventory/index.php
@@ -21,7 +21,7 @@
      <link rel="stylesheet" href="../../css/style_addInvenroty.css">
      <link rel="stylesheet" href="../../css/style_resetIosButtonStyle.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-     <script type="text/javascript" src="../../js/script_purchase.js" charset="UTF-8"></script>
+     <script type="text/javascript" src="../../js/script_addInvenroty.js" charset="UTF-8"></script>
      <meta http-equiv="Cache-Control" content="no-cache">
      <meta name="apple-mobile-web-app-capable" content="yes">
      <!--
